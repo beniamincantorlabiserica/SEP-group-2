46a6e04945d111d729d5409e69520488784d395a
MarsTyrant <chiritamarius818@gmail.com>
2023-11-05 13:13:26 +0100

    :wq

--- WorldOfZuul/Game.cs
+++ WorldOfZuul/Game.cs
@@ -16,18 +16,18 @@
             Room? outside = new("Outside", "You are standing outside the main entrance of the university. To the east is a large building, to the south is a computing lab, and to the west is the campus pub.");
             Room? theatre = new("Theatre", "You find yourself inside a large lecture theatre. Rows of seats ascend up to the back, and there's a podium at the front. It's quite dark and quiet.");
             Room? pub = new("Pub", "You've entered the campus pub. It's a cozy place, with a few students chatting over drinks. There's a bar near you and some pool tables at the far end.");
-            Room? savanna = new("Savanna", "Right next to you, there are some weird-looking plants, which don't belong in this area. On your left, you see some farmers with their livestock and your right there is an iron mine");
+            Room? lab = new("Lab", "You're in a computing lab. Desks with computers line the walls, and there's an office to the east. The hum of machines fills the room.");
             Room? office = new("Office", "You've entered what seems to be an administration office. There's a large desk with a computer on it, and some bookshelves lining one wall.");
 
-            outside.SetExits(null, theatre, savanna, pub); // North, East, South, West
+            outside.SetExits(null, theatre, lab, pub); // North, East, South, West
 
             theatre.SetExit("west", outside);
 
             pub.SetExit("east", outside);
 
-            savanna.SetExits(outside, office, null, null);
+            lab.SetExits(outside, office, null, null);
 
-            office.SetExit("west", savanna);
+            office.SetExit("west", lab);
 
             currentRoom = outside;
         }
---
+++ WorldOfZuul/Quest.cs
@@ -0,0 +1,18 @@
+namespace TheNaturesLastStand
+{
+    class Quest
+    {
+        private string? name;
+        private string? description;
+        private bool done;
+        public void theQuest()
+        {
+            Console.WriteLine("The Quest");
+        }
+        public bool IsDone()
+        {
+            return true;
+        }
+
+    }
+}
