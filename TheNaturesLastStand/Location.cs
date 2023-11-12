8bc7c4c65455ffb377597e38e78831c05ffd6e4b
MarsTyrant <chiritamarius818@gmail.com>
2023-11-12 09:33:34 +0100

    updatedLocationClass

--- TheNaturesLastStand/Location.cs
+++ TheNaturesLastStand/Location.cs
@@ -1,21 +1,35 @@
 
+using System.Data.Common;
+
 namespace TheNaturesLastStand
 {
    public class Location
    {
-      //  private Item[] items=new Item[20];
+      //  private Item item;
       public string name { get;}
-      public string description { get;}
-      public List<Quest> quests { set; get;}
-      private int currentAdvancements;
-      private int totalAdvancements;
-      public Location(string name, string description, int totalAdvancements, List<Quest> quests)
+      private Quest quest;
+      private Dictionary<string,Location> exits;
+      private int id;
+
+      public Location(string name)
       {
             this.name = name;
-            this.description = description;
-            this.totalAdvancements = totalAdvancements;
-            currentAdvancements = 0;
-            this.quests = quests;
       }
+      public void SetExits(Location? up, Location? down, Location? right, Location? left)
+      {
+         SetExit("up", up);
+         SetExit("down", down);
+         SetExit("left", left);
+         SetExit("right", right);
+
+      }
+      private void SetExit(string direction, Location? neighbor)
+      {
+         if(neighbor!=null)
+         {
+            exits[direction] = neighbor;
+         }
+      }
+
    }
 }
