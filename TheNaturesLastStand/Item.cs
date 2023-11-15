70fbd20285406e3b3b7bb502e6c78f9b2db5afb3
IonelaSorina <ionelapertea24@gmail.com>
2023-11-15 15:02:23 +0100

    ADDED Item class

---
+++ TheNaturesLastStand/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheNaturesLastStand
+{
+    abstract class Item
+    {
+        public abstract void Use();
+
+        public abstract void Pick();
+
+        public abstract void Drop(int locationId);
+         
+    }
+    
+}
+
