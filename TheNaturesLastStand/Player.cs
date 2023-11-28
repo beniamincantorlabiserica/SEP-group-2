bc6abb86d05c50213e233d9bf44be65b07c1ab9b
beniamin <avr.beniamin@icloud.com>
2023-11-28 02:05:40 +0100

    UPDATED player command logic from switch to for loops

--- TheNaturesLastStand/Player.cs
+++ TheNaturesLastStand/Player.cs
@@ -24,157 +24,165 @@ public class Player
 
     public void DoCommand(string Command)
     {
-        switch (Command)
+        if (Command == "move right")
         {
-            case "move right":
-                if (CurrentLocation.RightLocation != null)
-                {
-                    CurrentLocation = CurrentLocation.RightLocation;
-                    UpdateScreen(LocationSwitchMessage());
-                }
-                else
-                {
-                    UpdateScreen("Sorry...no location in this direction.");
-                }
-                break;
-            case "move left":
-                if (CurrentLocation.LeftLocation != null)
-                {
-                    CurrentLocation = CurrentLocation.LeftLocation;
-                    UpdateScreen(LocationSwitchMessage());
-                }
-                else
-                {
-                    UpdateScreen("Sorry...no location in this direction.");
-                }
-                break;
-            case "move up":
-                if (CurrentLocation.UpLocation != null)
-                {
-                    CurrentLocation = CurrentLocation.UpLocation;
-                    UpdateScreen(LocationSwitchMessage());
-                }
-                else
-                {
-                    UpdateScreen("Sorry...no location in this direction.");
-                }
-                break;
-            case "move down":
-                if (CurrentLocation.DownLocation != null)
-                {
-                    CurrentLocation = CurrentLocation.DownLocation;
-                    UpdateScreen(LocationSwitchMessage());
-                }
-                else
-                {
-                    UpdateScreen("Sorry...no location in this direction.");
-                }
-                break;  
-            case "accept":
-                CurrentLocation.Quest.State = QuestState.Active;
-                break;
-            case "decline":
-                if (CurrentLocation.Quest.Type == QuestType.NpcQuest)
-                {
-                    CurrentLocation.Quest.State = QuestState.Seen;
-                }
-                UpdateScreen("Sorry to heat that...guess somebody else has to save the world...");
-                break;
-            case CurrentLocation.Quest.PositiveCommand:
-                CurrentLocation.Quest.State = QuestState.Done;
-                Balance += CurrentLocation.Quest.RewardAmount;
-                UpdateScreen(CurrentLocation.Quest.Dialog[2]);
-                break;
-            case CurrentLocation.Quest.NegativeCommand:
-                UpdateScreen("Uhh...actually it would be nice of you if you did this...");
-                break;
-            case "tutorial": 
-                ScreenManager.DisplayTutorial(); 
-                break;
-            case "look":
-                if (CurrentLocation.Quest.Type == QuestType.NpcQuest)
-                {
-                    switch (CurrentLocation.Quest.State)
-                    {
-                        case QuestState.NotSeen: 
-                            CurrentLocation.Quest.State = QuestState.Seen;
-                            UpdateScreen($"Someone is here, try talking by using \"talk\" command.");
-                            break;
-                        default: 
-                            UpdateScreen($"Someone is here, try talking by using \"talk\" command.");
-                            break;
-                    }
-                }
-                else if(CurrentLocation.Quest.Type == QuestType.Regular)
-                {
-                    switch (CurrentLocation.Quest.State)
-                    {
-                        case QuestState.NotSeen:
-                            UpdateScreen(CurrentLocation.Quest.Description + "\n\nWould you like to accept this quest? \n\n >accept \n >decline)");
-                            CurrentLocation.Quest.State = QuestState.Seen;
-                            break;
-                        case QuestState.Seen:
-                            UpdateScreen(CurrentLocation.Quest.Description + "\n\nWould you like to accept this quest? \n\n >accept \n >decline)");
-                            CurrentLocation.Quest.State = QuestState.Seen;
-                            break;
-                        case QuestState.Active:
-                            UpdateScreen(CurrentLocation.Quest.Description + "\n\n >" + CurrentLocation.Quest.PositiveCommand + "\n >" + CurrentLocation.Quest.NegativeCommand);
-                            break;
-                        case QuestState.Done:
-                            UpdateScreen("You already completed this Quest. Try going to another location find more quests");
-                            break;
-                        default:
-                            
-                    }
-                }
-                break;
-            case "talk":
-                if (CurrentLocation.Quest.Type == QuestType.NpcQuest)
-                {
-                    switch (CurrentLocation.Quest.State)
-                    {
-                        case QuestState.Seen:
-                            UpdateScreen(CurrentLocation.Quest.Dialog[0] + "\n\n >accept\n >decline");
-                            CurrentLocation.Quest.State = QuestState.Talking;
-                            break;
-                        case QuestState.Active:
-                            UpdateScreen(CurrentLocation.Quest.Dialog[1]);
-                            break;
-                        case QuestState.Done:
-                            UpdateScreen("Your help was very welcome. Thank you for fighting for the planet!");
-                            break;
-                    }
-                }
-                break;
-            case "help":
-                ScreenManager.DisplayHelp();
-                break;
-            case "quit":
-                ScreenManager.DisplayGameOver();
-                break;
+            if (CurrentLocation.RightLocation != null)
+            {
+                CurrentLocation = CurrentLocation.RightLocation;
+                UpdateScreen(LocationSwitchMessage());
+            }
+            else
+            {
+                UpdateScreen("Sorry...no location in this direction.");
+            }
         }
+        else if (Command == "move left")
+        {
+            if (CurrentLocation.LeftLocation != null)
+            {
+                CurrentLocation = CurrentLocation.LeftLocation;
+                UpdateScreen(LocationSwitchMessage());
+            }
+            else
+            {
+                UpdateScreen("Sorry...no location in this direction.");
+            }
+        }
+        else if (Command == "move up")
+        {
+            if (CurrentLocation.UpLocation != null)
+            {
+                CurrentLocation = CurrentLocation.UpLocation;
+                UpdateScreen(LocationSwitchMessage());
+            }
+            else
+            {
+                UpdateScreen("Sorry...no location in this direction.");
+            }
+        }
+        else if (Command == "move down")
+        {
+            if (CurrentLocation.DownLocation != null)
+            {
+                CurrentLocation = CurrentLocation.DownLocation;
+                UpdateScreen(LocationSwitchMessage());
+            }
+            else
+            {
+                UpdateScreen("Sorry...no location in this direction.");
+            }
+        }
+        else if (Command == "accept")
+        {
+            if (CurrentLocation.Quest.Type == QuestType.NpcQuest)
+            {
+                if (CurrentLocation.Quest.State == QuestState.Talking)
+                {
+                    CurrentLocation.Quest.State = QuestState.Active;
+                    ActiveQuests.Add(CurrentLocation.Quest);
+                }
+                else
+                {
+                    InvalidCommand();
+                }
+            }
+            else
+            {
+                CurrentLocation.Quest.State = QuestState.Active;
+                ActiveQuests.Add(CurrentLocation.Quest);
+            }
+        }
+        else if (Command == "decline")
+        {
+            if (CurrentLocation.Quest.Type == QuestType.NpcQuest)
+            {
+                CurrentLocation.Quest.State = QuestState.Seen;
+            }
 
-
-
-       
-       
-
-        
-        
-        
-
-
-
-       
-        
-        
-
-        
-        
-        
-        
-        
-        
+            UpdateScreen("Sorry to heat that...guess somebody else has to save the world...");
+        }
+        else if (Command == CurrentLocation.Quest.PositiveCommand)
+        {
+            CurrentLocation.Quest.State = QuestState.Done;
+            Balance += CurrentLocation.Quest.RewardAmount;
+            UpdateScreen(CurrentLocation.Quest.Dialog[2]);
+        }
+        else if (Command == CurrentLocation.Quest.NegativeCommand)
+        {
+            UpdateScreen("Uhh...actually it would be nice of you if you did this...");
+        }
+        else if (Command == "tutorial")
+        {
+            ScreenManager.DisplayTutorial();
+        }
+        else if (Command == "look")
+        {
+            if (CurrentLocation.Quest.Type == QuestType.NpcQuest)
+            {
+                switch (CurrentLocation.Quest.State)
+                {
+                    case QuestState.NotSeen:
+                        CurrentLocation.Quest.State = QuestState.Seen;
+                        UpdateScreen($"Someone is here, try talking by using \"talk\" command.");
+                        break;
+                    default:
+                        UpdateScreen($"Someone is here, try talking by using \"talk\" command.");
+                        break;
+                }
+            }
+            else if (CurrentLocation.Quest.Type == QuestType.Regular)
+            {
+                switch (CurrentLocation.Quest.State)
+                {
+                    case QuestState.NotSeen:
+                        UpdateScreen(CurrentLocation.Quest.Description +
+                                     "\n\nWould you like to accept this quest? \n\n >accept \n >decline)");
+                        CurrentLocation.Quest.State = QuestState.Seen;
+                        break;
+                    case QuestState.Seen:
+                        UpdateScreen(CurrentLocation.Quest.Description +
+                                     "\n\nWould you like to accept this quest? \n\n >accept \n >decline)");
+                        CurrentLocation.Quest.State = QuestState.Seen;
+                        break;
+                    case QuestState.Active:
+                        UpdateScreen(CurrentLocation.Quest.Description + "\n\n >" +
+                                     CurrentLocation.Quest.PositiveCommand + "\n >" +
+                                     CurrentLocation.Quest.NegativeCommand);
+                        break;
+                    case QuestState.Done:
+                        UpdateScreen(
+                            "You already completed this Quest. Try going to another location find more quests");
+                        break;
+                }
+            }
+        }
+        else if (Command == "talk")
+        {
+            if (CurrentLocation.Quest.Type == QuestType.NpcQuest)
+            {
+                switch (CurrentLocation.Quest.State)
+                {
+                    case QuestState.Seen:
+                        UpdateScreen(CurrentLocation.Quest.Dialog[0] + "\n\n >accept\n >decline");
+                        CurrentLocation.Quest.State = QuestState.Talking;
+                        break;
+                    case QuestState.Active:
+                        UpdateScreen(CurrentLocation.Quest.Dialog[1]);
+                        break;
+                    case QuestState.Done:
+                        UpdateScreen("Your help was very welcome. Thank you for fighting for the planet!");
+                        break;
+                }
+            }
+        }
+        else if (Command == "help")
+        {
+            ScreenManager.DisplayHelp();
+        }
+        else if (Command == "quit")
+        {
+            ScreenManager.DisplayGameOver();
+        }
     }
 
     // TO-DO
