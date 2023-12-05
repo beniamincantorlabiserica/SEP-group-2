dfdce39f7e0dd6dfd05343b4cabab50fc2b5c312
bob <eduards.br7@gmail.com>
2023-12-05 15:54:50 +0100

    QuestClassDocumentation

--- TheNaturesLastStand/Quest.cs
+++ TheNaturesLastStand/Quest.cs
@@ -14,6 +14,19 @@ public class Quest
     public string PositiveCommand { get; set; }
     public string NegativeCommand { get; set; }
 
+    /// <summary>
+    /// Constructor of class Quest Initializing the attirbutes
+    /// </summary>
+    /// <param name="PositiveCommand">correct quest advancement command</param>
+    /// <param name="NegativeCommand">incorrect quest advancement command</param>
+    /// <param name="Dialog">an array storing quest dialogue</param>
+    /// <param name="RewardAmount">reward amount for completing the quest</param>
+    /// <param name="Name">quest name used for quest display</param>
+    /// <param name="Description">quest description for quest display</param>
+    /// <param name="Type">which of the quest types the quest is</param>
+    /// <param name="State">current quest state</param>
+    /// <param name="BiomeId">biome in which the quest is</param>
+    /// <param name="ID">specific quest id</param>
     public Quest(int ID, string positiveCommand, string negativeCommand, string[] dialog, int rewardAmount, string name, string description, int biomeId, QuestType type = QuestType.Regular)
     {
         PositiveCommand = positiveCommand;
